c35c5d72b1fc8e02f94887c831c717797b4da7df
Cecile <cecile.cadoul@epitech.eu>
2019-10-08 14:01:59 +0200

    [ADD] Adding Imgur connection redirection

--- Epicture/Epicture/ViewController.swift
+++ Epicture/Epicture/ViewController.swift
@@ -14,7 +14,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: Actions
+    @IBAction func connectWithImgur(_ sender: UIButton) {
+//        let authSession = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+//        let authURL = URL(string: "https://api.imgur.com/oauth2/authorize?client_id=3aab9940d90a6ac&response_type=token&state=test")!
+        
+        guard let url = URL(string: "https://api.imgur.com/oauth2/authorize?client_id=3aab9940d90a6ac&response_type=token&state=test") else {
+                return
+            }
+            UIApplication.shared.open(url)
+//        let authTask = authSession.dataTask(with: authURL, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+//            guard let response = response else {
+//                return
+//            }
+//            print("toto")
+//            print(response)
+//            print("tata")
+//        })
+//
+//        authTask.resume()
+    }
+    
 }
 
