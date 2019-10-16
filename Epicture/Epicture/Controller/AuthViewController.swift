91776ec24804a262bcf2f2f28bc612d9e388fb49
Florent Poinsard <florent.poinsard@outlook.fr>
2019-10-16 09:44:59 +0200

    [TEST] - Test integration button

--- Epicture/Epicture/Controller/AuthViewController.swift
+++ Epicture/Epicture/Controller/AuthViewController.swift
@@ -15,14 +15,47 @@ class AuthViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func testAuth(_ sender: UIButton) {
+                print(UserDefaults.standard.string(forKey: "refresh_token") as Any)
+                print(UserDefaults.standard.string(forKey: "access_token") as Any)
+                print(UserDefaults.standard.string(forKey: "account_username") as Any)
+                print(UserDefaults.standard.string(forKey: "account_id") as Any)
+    }
+
+
+    
+    @IBAction func logout(_ sender: UIButton) {
+                UserDefaults.standard.set("", forKey: "refresh_token")
+                UserDefaults.standard.set("", forKey: "access_token")
+                UserDefaults.standard.set("", forKey: "account_username")
+                UserDefaults.standard.set("", forKey: "account_id")
+    }
+    
+    @IBAction func testAPI(_ sender: UIButton) {
+                print("get avatar")
+                let client = ImgurAPIClient()
+        
+                do {
+                    let avatar_url = try client.getFavorites(username: UserDefaults.standard.string(forKey: "account_username")!)
+                    print(avatar_url)
+                } catch let err {
+                    print(err)
+                }
+        
+    }
+    
+    
     //MARK: Actions
     @IBAction func connectWithImgur(_ sender: UIButton) {
-//        guard let url = URL(string: "https://api.imgur.com/oauth2/authorize?client_id=3aab9940d90a6ac&response_type=token&state=test") else {
-//            return
-//        }
-//        UIApplication.shared.open(url)
+        let client = ImgurAPIClient()
+
+        guard let url = URL(string: "https://api.imgur.com/oauth2/authorize?client_id=" + client.ClientID + "&response_type=token&state=test") else {
+            return
+        }
+        UIApplication.shared.open(url)
         openTabBarController()
     }
+
     //MARK: Private function
     private func openTabBarController() {
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
